d77ca25921f040dc04e5b70e952cd2fca01b9376
loyolu <loyolu@users.noreply.github.com>
2014-07-11 16:24:11 -0500

    kitchen page working, created checklist.js & data.js, still need to fix kitchen header

--- js/checklist.js
+++ js/checklist.js
@@ -8,48 +8,50 @@
 
   console.log(CHECKLISTS);
 
-  $.each(CHECKLISTS[0].items, function( idx, item ) {
-  	console.log(idx, item);
-  	var checkboxdiv = "<div class='pull-right checkbox'>" + "<div class='check'><img class='checkmark mark' src='../../images/green-checkmark.png'></div>" + "<div class='x'><img class='xmark mark' src='../../images/x-mark.png'></div>" + "</div>"
-  	var itemElement = $("<li class='table-view-cell' id='"  + CHECKLISTS[0].housearea +"-"+ idx + "'>" + item.description + checkboxdiv + "</li>") ;
-    //items.push( item);
-  	$( ".table-view").append(itemElement);
-  });
+  var loadItemsAtIndex = function(itemIndex) {
+  	$.each(CHECKLISTS[itemIndex].items, function( idx, item ) {
+  		console.log(idx, item);
+  		var checkboxdiv = "<div class='pull-right checkbox'>" + "<div class='check'><img class='checkmark marks' src='../../images/green-checkmark.png'></div>" + "<div class='x'><img class='xmark marks' src='../../images/x-mark.png'></div>" + "</div>"
+  		var itemElement = $("<li class='todo-table table-view-cell' id='"  + CHECKLISTS[0].housearea +"-"+ idx + "'>" + "<span class='description'>" + item.description + "</span>" + checkboxdiv + "</li>") ;
+	    //items.push( item);
+	    $( ".table-view").append(itemElement);
+	});
 
 
-
-
-var boxstate = 0; //stores whether the checkbox is empty, ✓ or x
+	var boxstate = 0; //stores whether the checkbox is empty, ✓ or x
 	$(".check").hide();
 	$(".xmark").hide();
-$('.checkbox').click(function() {
-	if (boxstate === 0) {
-	console.log("checkbox was clicked. ✓");
-	$(".check").show();
-	$(".xmark").hide();
-	boxstate++;
-	return;
-	}
-	if (boxstate === 1) {
-	console.log("checkbox was clicked. x");
-	$(".check").hide();
-	$(".xmark").show();
-	boxstate++;
-	return;
-	}
-	if (boxstate === 2) {
-	console.log("checkbox cleared");
-	$(".check").hide();
-	$(".xmark").hide();
-	boxstate = 0;
-	return;
-	}
-});
+	$('.checkbox').click(function() {
+		if (boxstate === 0) {
+			console.log("checkbox was clicked. ✓");
+			$(this).find(".check").show();
+			$(this).find(".xmark").hide();
+			$(this).parent().css("background-color", "#595959");
+			boxstate++;
+			return;
+		}
+		if (boxstate === 1) {
+			console.log("checkbox was clicked. x");
+			$(this).find(".check").hide();
+			$(this).find(".xmark").show();
+			boxstate++;
+			return;
+		}
+		if (boxstate === 2) {
+			console.log("checkbox cleared");
+			$(this).find(".check").hide();
+			$(this).find(".xmark").hide();
+			boxstate = 0;
+			$(this).parent().css("background-color", "#ffdab9");
+			return;
+		}
+	});
+};
 
 /*
 $(this).parent();
 $(this).find(".")
 
-$(this).css("background", "#595959");
+
 //color:#7f7f7f
 */
