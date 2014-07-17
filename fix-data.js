34cf1508c7ec305f699bf0b941c5f044e350a1a7
loyolu <loyolu@users.noreply.github.com>
2014-07-17 11:16:16 -0500

    Added id to data.js, centered walkthrough slides, centered walkthrough buttons, added data storage

---
+++ fix-data.js
@@ -0,0 +1,332 @@
+CHECKLISTS = [
+{ 
+	"housearea":"kitchen",
+	"items": [
+	{ 
+		"description": "Install covers for stove and oven knobs.",
+		"completed": "0", 
+		"dropdown": "Prevents child from turning on stove or oven accidentally."
+	},
+	{ 
+		"description": "Install appliance latch for the oven door.",
+		"completed": "0", 
+		"dropdown": "Prevents child from opening a potentially hot oven."
+	},
+	{ 
+		"description": "Install stove guard to block access to burners.",
+		"completed": "0", 
+		"dropdown": "Barrier keeps small hands away from burners."
+	},
+	{ 
+		"description": "Install safety latches on refrigerators and freezer doors.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Forgo placemats and tablecloths.",
+		"completed": "0", 
+		"dropdown": "Prevents baby from yanking loose cloth and having items fall on them."
+	},
+	{ 
+		"description": "Remove magnets from the refrigerator.",
+		"completed": "0",
+		"dropdown": "Potential choking hazard."
+	},
+	{ 
+		"description": "When cooking always keep pot handles turned toward wall.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Always keep hot food and drinks away from table edge.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "When possible, cook using only back burners.",
+		"completed": "0",
+		"dropdown": ""
+	}
+	]
+}, 
+
+{ 
+	"housearea":"living room",
+	"items": [
+	{ 
+		"description": "Install foam bumpers on fireplace hearth and mantles.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Latch any cupboards, doors, and drawers within baby's reach.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Purchase baby-safe doorstops.",
+		"completed": "0",
+		"dropdown": ""
+		},
+	{ 
+		"description": "Move all electric cords behind furniture or under rugs.",
+		"completed": "0", 
+		"description": ""
+	},
+	{ 
+		"description": "Secure heavy furniture to walls.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Secure television to wall or place on tall sturdy furniture.",
+		"completed": "0", 
+		"dropdown": ""
+	},
+	{ 
+		"description": "Remove floor lamps.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Put baby gates at the top and bottom of every set of stairs.",
+		"completed": "0",
+		"description": ""
+	},
+	{ 
+		"description": "Block access to all floor heaters and radiators.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install plexiglass on stair and balacony rail spaces greater than four inches.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install fireplace screen around fireplace.",
+		"completed":"0", 
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install bumpers to sharp corners on furniture.",
+		"completed":"0",
+		"dropdown": ""
+	}
+	]
+}
+,
+
+{ 
+	"housearea":"nursery",
+	"items": [
+	{ 
+		"description": "Purchase a crib with fixed sides.",
+		"completed": "0",
+		"dropdown": "Drop-sided cribs pose a threat to infants, many of whom have been injured or even killed as a result of hardware malfunctions common to the crib's design."
+	},
+	{ 
+		"description": "Crib has narrow slats.",
+		"completed": "0",
+		"dropdown": "Slats should be no further than 2 and 3/8th inches apart."
+	},
+	{ 
+		"description": "Crib does not have corner posts.",
+		"completed": "0",
+		"dropdown": "Clothing may become caught on posts, resulting in strangulation."
+	},
+	{ 
+		"description": "Crib mattress is firm and well-fitting.",
+		"completed": "0",
+		"dropdown": "There should be no more than two fingers of space between the side of the mattress and the crib frame. Any larger and your baby may become entrapped between the two, resulting in injury or suffocation."
+	},
+	{ 
+		"description": "Remove soft toys and bedding from crib.",
+		"completed": "0",
+		"dropdown": "Blankets, pillows and bumpers of any kind pose a risk of suffocation and or entrapment and should never be used in a crib."
+	},
+{ 
+		"description": "Leave only thin mattress, fitted sheet in crib.",
+		"completed": "0",
+		"dropdown": "Blankets, pillows and bumpers of any kind pose a risk of suffocation and or entrapment and should never be used in a crib."
+	},
+	{ 
+		"description": "Once baby can sit up, remove hanging mobiles from crib.",
+		"completed": "0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Move crib away from anything that could be used for climbing.",
+		"completed":"0",
+		"dropdown": ""
+	},
+{ 
+		"description": "Ensure crib is not next to a heater or an area with direct sunlight.",
+		"completed": "0",
+		"dropdown": "Both are risk factors for SIDS."
+	},
+	{ 
+		"description": "If using a glider, select one with a stop-lock mechanism.",
+		"completed": "0",
+		"description": "Prevents glider from moving when not in use."
+	}
+	]
+}
+,
+
+{ 
+	"housearea":"outdoors",
+	"items": [
+	{ 
+		"description": "Ensure backyard gates latch securly.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Empty wading pools and store upright after each use.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install locked fence around permanent pool structures.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Ensure ground cover is soft (ie. grass or rubber). Avoid pea gravel at all costs.",
+		"completed":"0",
+		"dropdown": "Pea gravel is a choking hazard."
+	},
+	{ 
+		"description": "Avoid dangerous plants.",
+		"completed":"0",
+		"dropdown": "Including those that are toxic (mushrooms), prickly (rose, bougainvillea), bear fruits (berries)."
+	},
+	{ 
+		"description": "Ensure fertilizers and insecticides are safe during and after application.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Store garden hoses out of the sun.",
+		"completed":"0",
+		"dropdown": "Water remaining in hose can heat and scald child."
+	}
+
+	]
+}
+,
+{ 
+	"housearea":"garage",
+	"items": [
+	{ 
+		"description": "Store tools out of reach and don't allow electrical cords to dangle.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Make sure garage door has a safety sensor.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Put toxic substances in locked storage.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install lock on door connecting the house to the garage.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install lock on any entryways leading to the basement.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install tall flags on riding toys and ensure riding area is safe.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Store buckets upside down to prevent liquid accumulation.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Keep recyling container and garbage can closed and locked.",
+		"completed":"0",
+		"dropdown": ""
+	}
+
+	]
+}
+,
+
+{ 
+	"housearea":"bathroom",
+	"items": [
+	{ 
+		"description": "Secure cabinets with cabinet locks.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Secure toilet with toilet locks.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Ensure medications are out of reach and secured with childproof tops.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install a spout cover in the bathtub.",
+		"completed":"0",
+		"dropdown": "Note: bathtubs spouts are the third most common cause of stitches in babies."
+	},
+	{ 
+		"description": "Put non-slip mats in and beside the bathtub.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install safety locks on toilets.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Reduce water heater settings to 120 degrees Farenheit.",
+		"completed":"0",
+		"dropdown": ""
+	},
+	{ 
+		"description": "Install a stable shower curtain rod.",
+		"completed": "0",
+		"dropdown": ""
+	}
+	]
+}
+];
+
+
+var uuid = require('uuid');
+var fs = require('fs');
+
+for ( var i=0; i < CHECKLISTS.length; i++){
+	var area = CHECKLISTS[i];
+
+	console.log("processing", area.housearea);
+	for(var j=0; j< area.items.length; j++){
+		area.items[j].id = uuid.v4();
+	}
+}
+
+fs.writeFileSync('data-preocessed.json', JSON.stringify(CHECKLISTS));
+
+
+
+
+
--- js/checklist.js
+++ js/checklist.js
@@ -6,45 +6,99 @@
   // var garageitems = [];
   // var bathitems = [];
 
-  console.log(CHECKLISTS);
+//ERRORS - Checklists is not defined
+// boxstate is not defined
+// undefined is not a function (index.html:41)
+  	loadData();
+
+function loadData() {
+	var savedData = localStorage.getItem("CHECKLISTS");
+	if (savedData){
+		CHECKLISTS = JSON.parse(savedData);
+	}
+	else {
+		CHECKLISTS = INIT_CHECKLISTS;
+		saveData();
+	}
+}
+
+function saveData () {
+	// save data to local storage
+	localStorage.setItem("CHECKLISTS",JSON.stringify(CHECKLISTS));
+}
+
+ console.log(CHECKLISTS);
 
   var loadItemsAtIndex = function(itemIndex) {
-  	$.each(CHECKLISTS[itemIndex-1].items, function( idx, item ) {
-  		console.log(idx, item);
-  		console.log(itemIndex);
-  		var checkboxdiv = "<div class='pull-right checkbox'>" + "<div class='check'><img class='checkmark marks' src='../../images/green-checkmark.png'></div>" + "<div class='x'><img class='xmark marks' src='../../images/x-mark.png'></div>" + "</div>"
+  	var itemList = CHECKLISTS[itemIndex-1].items;
+  	$.each(itemList, function( idx, item ) {
+  		//console.log(idx, item);
+  		//console.log(itemIndex);
+  	 	//boxstate = parseInt(item.completed); //stores whether the checkbox is empty, ✓ or x
+  		var checkboxdiv = "<div class='pull-right checkbox' id= '"+ item.id + "'>" + "<div class='check'><img class='checkmark marks' src='../../images/green-checkmark.png'></div>" + "<div class='x'><img class='xmark marks' src='../../images/x-mark.png'></div>" + "</div>"
   		var itemElement = $("<li class='todo-table table-view-cell' id='"  + CHECKLISTS[0].housearea +"-"+ idx + "'>" + "<span class='description'>" + item.description + "</span>" + checkboxdiv + "</li>") ;
-	    //items.push( item);
 	    $( ".table-view").append(itemElement);
 	});
 
+	for(var i=0; i<itemList.length; i++){
+		if (itemList[i].completed == 0){
+			var div = $("#"+itemList[i].id);
+			div.find('.check').hide();
+			div.find('.xmark').hide();
+			div.parent().css("background-color", "#ffdab9");
 
-	var boxstate = 0; //stores whether the checkbox is empty, ✓ or x
-	$(".check").hide();
-	$(".xmark").hide();
+		}
+		if (itemList[i].completed == 1){
+			var div = $("#"+itemList[i].id);
+			div.find('.xmark').hide();
+			div.parent().css("background-color", "#595959");
+		}
+		if (itemList[i].completed == 2){
+			var div = $("#"+itemList[i].id);
+			div.find('.check').hide();
+			div.parent().css("background-color", "#595959");
+		}
+	}
+
+
+
 	$('.checkbox').click(function() {
-		if (boxstate === 0) {
+
+		var itemID = $(this).attr('id');
+		var item = null;
+		for(var i=0; i<itemList.length; i++){
+			if (itemList[i].id === itemID){
+				item = itemList[i];
+			}
+		}
+
+		
+		item.completed = parseInt(item.completed);
+		if (item.completed  === 0) {
 			console.log("checkbox was clicked. ✓");
 			$(this).find(".check").show();
 			$(this).find(".xmark").hide();
 			$(this).parent().css("background-color", "#595959");
-			boxstate++;
+			item.completed = item.completed+1;
+			saveData();
 			return;
 		}
-		if (boxstate === 1) {
+		if (item.completed === 1) {
 			console.log("checkbox was clicked. x");
 			$(this).find(".check").hide();
 			$(this).find(".xmark").show();
-			boxstate++;
+			item.completed = item.completed+1;
+			saveData();
 			return;
 		}
-		if (boxstate === 2) {
+		if (item.completed === 2) {
 			console.log("checkbox cleared");
 			$(this).find(".check").hide();
 			$(this).find(".xmark").hide();
-			boxstate = 0;
+			item.completed = 0;
 			$(this).parent().css("background-color", "#ffdab9");
+			saveData();
 			return;
 		}
 	});
-};
+}
